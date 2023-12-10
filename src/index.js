2231bfeb0946b78cc0134d2ddd4b09cfaef2a1ce
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2023-12-10 12:41:57 -0300

    refactor: Se elimina la parte de la api para solo dejar funcionalidad de scrapper

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import getAllcincoNorte from './tiendas/cincoNorte/index.js'
+import getAllCrossmountain from './tiendas/crossmountain/index.js'
+import getAllDecathlon from './tiendas/decathlon/index.js'
 
-const app = new Hono()
+const getAll = async () => {
+  await getAllcincoNorte()
+  await getAllCrossmountain()
+  await getAllDecathlon()
+}
 
-app.use('/api/*', cors())
-app.get('/', (c) => {
-  return c.text('API de scrapping de productos de ciclismo en tiendas en linea en Chile.')
-})
-
-app.get('/api/5norte', (c) => {
-  return c.json({
-    tienda: '5norte',
-    productos: {
-    }
-  })
-})
-
-app.get('/api/crossmountain', (c) => {
-  return c.json({
-    tienda: 'Crossmountain',
-    productos: {
-    }
-  })
-})
-
-app.get('/api/decathlon', (c) => {
-  return c.json({
-    tienda: 'Decathlon',
-    productos: {
-    }
-  })
-})
-
-export default app
+getAll()
